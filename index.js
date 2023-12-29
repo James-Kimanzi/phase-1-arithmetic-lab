96a42aae59b30b32a1ad31bc6dd9d32ae68c0b83
James <jameskimanzi@gmail.com>
2023-12-29 19:33:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let multiply;
+let num1 = 31;
+let num2 = 2;
+multiply = num1 * num2;
+
+let random;
+random = Math.round(Math.random()) +1
+
+let mod;
+let num3 = 30;
+let num4 = 13;
+mod = num3 % num4;
+
+let max;
+max = Math.max(10, 20, 15, 16, 9);
+
 
 // Write your code here
